f41bb813b1fc5568b931fa1cd11d67148aa184f9
kiendn1 <trungkienty2001@gmail.com>
2024-12-24 10:38:30 +0000

    fix main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -14,23 +14,17 @@ U=$5 # coefficient for loss_u
 NAME=$6 # job name
 
 
-for SEED in 42
-do
-    DIR=output/${DATASET}/${TRAINER}/${CFG}/${T}_${TAU}_${U}_${NAME}/a2c/seed_${SEED}
-    if [ -d "$DIR" ]; then
-        echo "Results are available in ${DIR}. Skip this job"
-    else
-        echo "Run this job and save the output to ${DIR}"
-        # srun -J testit -N 1 -p RTX2080Ti --gres gpu:1 --priority 9999999 \
-        python train.py \
-        --root "/kaggle/input/oh-data" \
-        --seed ${SEED} \
-        --trainer ${TRAINER} \
-        --dataset-config-file configs/datasets/${DATASET}.yaml \
-        --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
-        --output-dir ${DIR} \
-        TRAINER.DAPL.T ${T} \
-        TRAINER.DAPL.TAU ${TAU} \
-        TRAINER.DAPL.U ${U} &
-    fi
-done
+SEED=42
+
+DIR=output/${DATASET}/${TRAINER}/${CFG}/${T}_${TAU}_${U}_${NAME}/a2c/seed_${SEED}
+
+python train.py \
+--root "/kaggle/input/oh-data" \
+--seed ${SEED} \
+--trainer ${TRAINER} \
+--dataset-config-file configs/datasets/${DATASET}.yaml \
+--config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+--output-dir ${DIR} \
+TRAINER.DAPL.T ${T} \
+TRAINER.DAPL.TAU ${TAU} \
+TRAINER.DAPL.U ${U} &
