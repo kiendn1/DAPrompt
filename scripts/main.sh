1e1b37c88a2d7cfde92e84d68be52fbddb38799b
kiendn1 <trungkienty2001@gmail.com>
2024-12-24 10:14:07 +0000

    a2c settings

--- scripts/main.sh
+++ scripts/main.sh
@@ -3,7 +3,7 @@
 cd ..
 
 # custom config
-DATA=/home/data # you may change your path to dataset here
+DATA= "/kaggle/input/oh-data" # you may change your path to dataset here
 TRAINER=DAPL
 
 DATASET=$1 # name of the dataset
@@ -14,16 +14,16 @@ U=$5 # coefficient for loss_u
 NAME=$6 # job name
 
 
-for SEED in 1 2 3 4 5
+for SEED in 42
 do
-    DIR=output/${DATASET}/${TRAINER}/${CFG}/${T}_${TAU}_${U}_${NAME}/seed_${SEED}
+    DIR=output/${DATASET}/${TRAINER}/${CFG}/${T}_${TAU}_${U}_${NAME}/a2c/seed_${SEED}
     if [ -d "$DIR" ]; then
         echo "Results are available in ${DIR}. Skip this job"
     else
         echo "Run this job and save the output to ${DIR}"
         srun -J testit -N 1 -p RTX2080Ti --gres gpu:1 --priority 9999999 \
         python train.py \
-        --root ${DATA} \
+        --root "/kaggle/input/oh-data" \
         --seed ${SEED} \
         --trainer ${TRAINER} \
         --dataset-config-file configs/datasets/${DATASET}.yaml \
